f989a8e50f57df9f993d1836e9dbffe528541cd4
Adi Chirilov <developer@rosoftware.ro>
2020-01-15 16:04:14 +0200

    Django Dashboard Shards - Initial Commit

---
+++ core/static/assets/js/app/app-blog-new-post.1.1.0.js
@@ -0,0 +1,29 @@
+/*
+ |--------------------------------------------------------------------------
+ | Shards Dashboards: Blog Add New Post Template
+ |--------------------------------------------------------------------------
+ */
+
+'use strict';
+
+(function ($) {
+  $(document).ready(function () {
+
+    var toolbarOptions = [
+      [{ 'header': [1, 2, 3, 4, 5, false] }],
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent                                       // remove formatting button
+    ];
+
+    // Init the Quill RTE
+    var quill = new Quill('#editor-container', {
+      placeholder: 'Words can be like x-rays if you use them properly...',
+      theme: 'snow'
+    });
+
+  });
+})(jQuery);
